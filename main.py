6a218f512a174c2e62ca18c746305af47c39f598
naman pandey <naman03mgs@gmail.com>
2026-08-20 20:28:13 +0530

    learned about variables in easy and simple language

--- main.py
+++ main.py
@@ -18,3 +18,32 @@ print(type(a))
 b = 12.5;
 print(b)
 print(type(b))
+
+# variables 
+
+#variables can be defined as a name given to memory located in a program, here are some examples for the variables 
+# variable with few steps 
+#step 1 -> to use variable first we store the data in memory 
+
+name = "ram" 
+
+# in this sentence the "name" represents the variable and the "ram" represents the value of the variable 
+
+# step 2 -> now give command to print name by given variable 
+
+print(name) #ram
+
+# lets take more examples of 
+
+#eg 1 ->
+age = 12;
+colour = "green"
+hobby = "badminton"
+
+# in these 3 sentences above the "age", "colour" and the "hobby" are the variable and the "12", "green" and "badminton" are the values which are stored in the memory 
+
+# now we can give command to print the variables to check if the values are stored in the memory or not 
+
+print(age) # 12
+print(colour) #green
+print(hobby) #badminton
