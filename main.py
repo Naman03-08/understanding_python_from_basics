5cf589c3c395f53ae861ff5ca421d8a6cc405dbe
naman pandey <naman03mgs@gmail.com>
2026-08-20 20:12:20 +0530

    learned python by adding more 2 sentenceses

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 print("hello world")
+
+# printing few sentences to understand python more clearely 
+
+print("python is very easy and high level language")
+print("python is not a case sensitive language")
