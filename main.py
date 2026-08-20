3d66266ff9d0885cfc1b56d06d551f5bf9d6e8fa
naman pandey <naman03mgs@gmail.com>
2026-08-20 20:16:36 +0530

    learned about how to print number and there type

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+#lets start learning python in very simple and easy language so our first line of code looks like this 
+
 print("hello world")
 
 # printing few sentences to understand python more clearely 
 
 print("python is very easy and high level language")
 print("python is not a case sensitive language")
+
+# in python we can also print numbers in and those are concidred either integer or floating value according to the value of the number
+
+a = 12; # (an integer value)
+print(a)
+print(type(a))
+
+# for a floating value with proof of its floating type
+
+b = 12.5;
+print(b)
+print(type(b))
